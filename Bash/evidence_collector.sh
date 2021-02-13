4a00b5f9127599bc35d19a673230d98af0a4182e
Taz Wake <26778008+TazWake@users.noreply.github.com>
2021-02-13 18:36:25 +0000

    Create evidence_collector.sh
    Initial ideas

---
+++ Bash/evidence_collector.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Evidence collection script for Linux hosts
+#
+# Useage:
+# Run script to copy memory and disk image to external storage media.
+#
+
+# Get storage path
+
+# Grab memory
+
+# Collect network data
+
+# Get disk image
+
+
