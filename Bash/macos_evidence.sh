aaf5daa503f49dbf1e22755b2f2af87dc5b08b2c
Taz Wake <t.wake@halkynconsulting.co.uk>
2021-07-24 10:30:46 +0100

    MacOS collection script started

---
+++ Bash/macos_evidence.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+# This script is a starter-script for capturing
+# data from a suspected-compromised MacOS host.
+#
+# This is no a Forensic Evidence script. It is
+# designed to support incident response.
+#
+# SYNTAX
+#
+# scriptname.sh <storage location>
+
+# Set up environment
+$STORE="$1"
+
+
+#  Check it is run as root
+if  [[ $EUID -ne 0 ]]; then
+    echo "This script requires root privileges to run"
+    exit
+fi
+
+sudo -k
