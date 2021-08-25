af547c298891aec5c18a3b105dec04b9eee5ab54
Taz Wake <t.wake@halkynconsulting.co.uk>
2021-08-25 11:15:50 +0100

    Added folder logic

--- Bash/exifevidence.sh
+++ Bash/exifevidence.sh
@@ -5,23 +5,46 @@
 # that have been modified during the time of an incident and then
 # checks to see if the last modified by field looks odd.
 
-FILE=$1
-
-lastmod=$(exiftool $FILE | grep "Last Modified By" | cut -d':' -f2)
-author=$(exiftool $FILE | grep "Creator" | cut -d':' -f2)
-modtime=$(exiftool $FILE | grep "File Modif" | cut -d':' -f2)
+# USE
+# ./exifevidence.sh /path/to/folder
 
 
-if [[ "$lastmod" != "$author" ]]
-then
-    echo "Note: $FILE, was last modified by $author, not its creator."
-    echo "This file was modified on $modtime."
-else
-    echo "The documnet $FILE, was last modified by its author."
-    echo "This file was modified on $modtime."
-fi
+#FILE=$1
+FOLDER="$1"
+
+find $FOLDER -type f -name "*.*" -exec sh -c '
+    for FILE do
+        # echo "Found $FILE"
+        lastmod=$(exiftool "$FILE" | grep "Last Modified By" | cut -d':' -f2)
+        author=$(exiftool "$FILE" | grep "Creator" | cut -d':' -f2)
+        modtime=$(exiftool "$FILE" | grep "File Modif" | cut -d':' -f2-)
+        if [ "$lastmod" != "$author" ]
+        then
+            echo "Note: $FILE, was last modified by $author, not its creator."
+            echo "This file was modified on $modtime."
+        else
+            echo "The documnet $FILE, was last modified by its author."
+            echo "This file was modified on $modtime."
+        fi
+    done
+' exec-sh {} +
+
 
 # TODO
 
 # 1. Add logic to search for modification timestamps in a date range
-# 2. Add logic to search directories not specific files 
+# 2. Add logic to search directories not specific files *DONE*
+
+
+#    echo "Checking $FILE"
+#    lastmod=$(exiftool "$FILE" | grep "Last Modified By" | cut -d':' -f2)
+#    author=$(exiftool $FILE | grep "Creator" | cut -d':' -f2)
+#    modtime=$(exiftool $FILE | grep "File Modif" | cut -d':' -f2)
+#    if [[ "$lastmod" != "$author" ]]
+#    then
+#        echo "Note: $FILE, was last modified by $author, not its creator."
+#        echo "This file was modified on $modtime."
+#    else
+#        echo "The documnet $FILE, was last modified by its author."
+#        echo "This file was modified on $modtime."
+#    fi
