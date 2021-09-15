330059cda76311da975e6bf94fac156e08e38631
Taz Wake <t.wake@halkynconsulting.co.uk>
2021-09-15 13:54:48 +0100

    Script created
    Create script to mount apfs formatted evidence images on Ubuntu based distros.

---
+++ Bash/apfs_setup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# #######################################
+# # This script sets up an Ubuntu-based #
+# # Linux system to be able to mount    #
+# # and examine evidence images in APFS #
+# # format.                             #
+# #######################################
+
+#
+# Use:
+# ./apfs_setup.sh
+#
+# Notes:
+# This script must be run with root permissions.
+# This script only works on systems that use apt.
+
+# Check Requirements
+if [[ $EUID != 0 ]]; then
+    echo "[!] This script must be run with root privileges!"
+    echo "[!] Exiting"
+    exit 255;
+else
+    echo "Initial check completed, script is running with correct privileges."
+fi
+
+# Update repos.
+echo "Updating repositories. You might get prompted for answers."
+apt update
+apt install libicu-dev bzip2 cmake libz-dev libbz2-dev fuse3 libfuse3-3 libfuse3-dev clang git libattr1-dev git -y
+echo "Requirement installation complete."
+
+# Install apfs-fuse
+echo "Installing apfs-fuse."
+cd /opt
+git clone https://github.com/sgan81/apfs-fuse.git
+cd apfs-fuse
+git submodule init
+git submodule update
+
+# Build
+echo "Building apfs-fuse"
+mkdir build
+cd build
+cmake ..
+make
+
+# Set up links
+echo "build complete"
+ln /opt/afps-fuse/build/apfs-dump /usr/bin/apfs-dump
+ln /opt/afps-fuse/build/apfs-dump-quick /usr/bin/apfs-dump-quick
+ln /opt/afps-fuse/build/apfs-fuse /usr/bin/apfs-fuse
+ln /opt/afps-fuse/build/apfsutil /usr/bin/apfsutil
+echo "Script complete. Apfs-fuse has been built and binaries linked to /usr/bin."
