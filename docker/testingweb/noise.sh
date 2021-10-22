4f37714bdac7c245b1b1459979bb796b3493c265
Taz Wake <t.wake@halkynconsulting.co.uk>
2021-10-22 22:30:34 +0100

    Create noise.sh

---
+++ docker/testingweb/noise.sh
@@ -0,0 +1,49 @@
+!/bin/bash
+
+echo "[ ] Started"
+for i in {1..14}
+do
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+    sleep 0.1
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.3" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15" http://127.0.0.1:9999/ & 2>/dev/null
+    sleep 0.1
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.3" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:93.0) Gecko/20100101 Firefox/93.0" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36" http://127.0.0.1:9999/ & 2>/dev/null
+    sleep 0.2
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15" http://127.0.0.1:9999/ & 2>/dev/null
+    curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.3" http://127.0.0.1:9999/ & 2>/dev/null
+    sleep 0.1
+done
+sleep 0.1
+for i in {1..20}
+do
+    for i in {1..6}
+    do
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+        sleep 0.1
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.3" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36" http://127.0.0.1:9999/ & 2>/dev/null
+        sleep 0.2
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15" http://127.0.0.1:9999/ & 2>/dev/null
+        curl -H "User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.3" http://127.0.0.1:9999/ & 2>/dev/null
+    done
+    sleep 0.2
+done
+
+echo "[ ] Finished"
