8b5a6bddf5209285e1f57bc052a63ec6f3dedcd4
Taz Wake <t.wake@halkynconsulting.co.uk>
2022-02-04 16:06:14 +0000

    Update collectEvidence.ps1

--- Powershell/collectEvidence.ps1
+++ Powershell/collectEvidence.ps1
@@ -16,3 +16,38 @@ Exectution:
     2. Navigate to the USB.
     3. .\collectEvidence.ps1
 #>
+
+write-host -ForegroundColor Blue "++++++++++++++++++++++++++"
+write-host -ForegroundColor DarkCyan "+ Triage Data Collection +"
+write-host -ForegroundColor DarkCyan "+   Starting Collection  +"
+write-host -ForegroundColor DarkCyan "+       @tazwake         +"
+write-host -ForegroundColor Blue "++++++++++++++++++++++++++"
+# Set up evidence and logging
+mkdir Evidence\$env:COMPUTERNAME-force
+Write-host -ForegroundColor Gray 'Artifact collection initiated at' $(get-date)
+Set-Content -Path \Evidence\$env:COMPUTERNAME\log.txt -Value "Evidence collection started: $((Get-Date).ToString())"
+Add-Content -Path \Evidence\$env:COMPUTERNAME\log.txt -Value "Initial folder created."
+
+Add-Content -Path \Evidence\$env:COMPUTERNAME\log.txt -Value "Memory capture initiated: $((Get-Date).ToString())"
+.\MRC.exe /accepteula /go /silent
+Start-Sleep -Seconds 3
+Write-host -ForegroundColor Yellow "[ ] Launching Magnet RAM Capture to collect a memory image."
+Write-host -ForegroundColor Yellow "[!] This may take a long time. "
+Wait-Process -name "MRC"
+Write-host -ForergroundColor DarkYellow "[X] Capture complete, logging OS build data. Renaming evidence."
+[System.Environment]::OSVersion.Version > .\Evidence\$env:COMPUTERNAME\OS_build_version.txt
+Get-ChildItem -Filter 'MagnetRAMCapture*' -Recurse | Rename-Item -NewName {$_.name -replace 'MagnetRAMCapture', $env:COMPUTERNAME }
+Move-Item -Path .\*.txt -Destination .\Evidence\$env:COMPUTERNAME\
+Move-Item -Path .\*.raw -Destination .\Evidence\$env:COMPUTERNAME\
+Get-FileHash -Algorithm MD5 .\Evidence\$env:COMPUTERNAME\*.raw | Out-File .\Evidence\$env:COMPUTERNAME\MemoryCapture_MD5Hashes.txt
+Write-host -ForergroundColor Yellow "[ ] Memory capture completed."
+Add-Content -Path \Evidence\$env:COMPUTERNAME\log.txt -Value "Memory capture completed: $((Get-Date).ToString())"
+
+Start-Sleep -Seconds 1
+Add-Content -Path \Evidence\$env:COMPUTERNAME\log.txt -Value "KAPE triage date collection initiated: $((Get-Date).ToString())"
+Write-host -ForegroundColor Green "[ ] Collecting OS Artifacts."
+.\Kape\kape.exe --tsource C: --tdest .\Evidence\$env:COMPUTERNAME --target !SANS_Triage --vhdx $env:COMPUTERNAME --zv false
+Set-Content -Path \Evidence\$env:COMPUTERNAME\Finished.txt -Value "Evidence collection complete: $((Get-Date).ToString())"
+Get-FileHash -Algorithm MD5 .\Evidence\$env:COMPUTERNAME\*.vhdx | Out-File .\Evidence\$env:COMPUTERNAME\KapeOutput_MD5Hashes.txt
+Add-Content -Path \Evidence\$env:COMPUTERNAME\log.txt -Value "Collection completed: $((Get-Date).ToString())"
+Write-Host -ForegroundColor Green "[ ] Collection completed"
