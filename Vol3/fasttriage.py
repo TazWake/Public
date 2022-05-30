eb95ea1e74a0c128d0858610cd43870f676a3840
Taz Wake <t.wake@halkynconsulting.co.uk>
2022-05-30 17:30:58 +0100

    Added logic
    Improved detection logic

--- Vol3/fasttriage.py
+++ Vol3/fasttriage.py
@@ -3,7 +3,7 @@ from typing import List
 from volatility3.framework import renderers, interfaces
 from volatility3.framework.configuration import requirements
 from volatility3.framework.interfaces import plugins
-from volatility3.plugins.windows import pslist
+from volatility3.plugins.windows import pslist, dlllist
 
 class FastTriage(plugins.PluginInterface):
     _required_framework_version = (2, 0, 0)
@@ -26,6 +26,7 @@ class FastTriage(plugins.PluginInterface):
                                    ("Image", str),
                                    ("Handles", int),
                                    ("Threads", int),
+                                   ("File Path", str),
                                    ("Status", str)
                                    ], self._generator(tasks))
 
@@ -33,8 +34,9 @@ class FastTriage(plugins.PluginInterface):
         lsasscount = 0
         smsscount = 0
         for task in data:
-            status=""
-            bypass=""
+            status="" # set variables before use
+            bypass="" # set variables before use
+            filepath="" # set variables before use
             # Check for short filenames
             procname = str(task.ImageFileName.cast("string",max_length = task.ImageFileName.vol.count, errors= 'replace'))
             if "." not in procname:
@@ -42,28 +44,61 @@ class FastTriage(plugins.PluginInterface):
             else:
                 exename, extension = procname.split('.') # Split the filename
                 if len(exename) < 3: 
-                    status = "Short filename"
+                    status = status + "Short filename. "
+            # Establish file path
+            for entry in task.load_order_modules():
+                BaseDllName = FullDllName = renderers.UnreadableValue() # Slightly more complex than needed to allow for future development
+                try:
+                    BinaryName = entry.BaseDllName.get_string()
+                    BinaryPath = entry.FullDllName.get_string()
+                    if "exe" in BinaryName:
+                        filepath = BinaryPath
+                        filepath = filepath.lower()
+                except exceptions.InvalidAddressException:
+                    pass
             # check for extra lsass
             filename = "lsass.exe"
-            if procname == filename:
+            if procname.lower() == filename:
                lsasscount = lsasscount+1
                if lsasscount > 1:
-                   status = "Multiple instances of lsass"
+                   status = "Multiple instances of lsass. "
+               if "\\windows\\system32\\lsass.exe" not in filepath:
+                   status = status + "Unusual path. "
             # Check for impersonation
+            # CSRSS
+            filename = "csrss.exe"
+            if procname.lower() == filename:
+                if "\\windows\\system32\\csrss.exe" not in filepath:
+                    status = status + "Unusual path. "
             check = ["cssrss.exe", "cssrs.exe", "csrs.exe", "crss.exe", "csrsss.exe", "csrrs.exe"]
             if procname.lower() in check:
-                status = "Possible impersonation of csrss.exe"
+                status = status + "Possible impersonation of csrss.exe. "
+            # SVCHOST 
+            filename = "svchost.exe"
+            if procname.lower() == filename:
+                if "\\windows\\system32\\svchost.exe" not in filepath:
+                    status = status + "Unusual path. "
             check = ["scvhost.exe", "svch0st.exe", "sscvhost.exe", "svcchost.exe", "scvh0st.exe", "svchozt.exe", "svchot.exe", "scvhot.exe", "svcnot.exe", "scvnot.exe", "svcnost.exe", "scvnost.exe"]
             if procname.lower() in check:
-                status = "Possible impersonation of svchost.exe"
+                status = status + "Possible impersonation of svchost.exe. "
+            # DLLHOST
+            filename = "dllhost.exe"
+            if procname.lower() == filename:
+                if "\\Windows\\System32\\dllhost" not in filepath:
+                    status = status + "Unusual path. "
             check = ["dllh0st.exe", "dllhot.exe", "d1lhost.exe", "dl1host.exe", "d11host.exe", "d11h0st.exe", "diihost.exe", "diih0st.exe", "dilhost.exe", "dlihost.exe"]
-            if procname in check:
-                status = "Possible impersonation of dllhost.exe"
-            
+            if procname.lower() in check:
+                status = status + "Possible impersonation of dllhost.exe. "
+            # Check for temp folder use
+            tempfolders = ["temp","appdata\\local", "appdata\\roaming", "spool\\drivers\\color", "tmp"] # Add folders as needed
+            for fp in tempfolders:
+                if fp in filepath:
+                    status = status + "Possible temp folder. "
             yield (0, [
                    int(task.UniqueProcessId),
                    procname,
                    task.get_handle_count(),
                    task.ActiveThreads,
+                   filepath,
                    status
                    ])
