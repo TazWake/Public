f77636a08459a6cba13c0e06b6559bc5d15b27cd
Taz Wake <t.wake@halkynconsulting.co.uk>
2023-08-17 09:36:19 +0100

    Update timestampCheck.sh

--- Bash/timestampCheck.sh
+++ Bash/timestampCheck.sh
@@ -4,7 +4,7 @@ clear
 echo "[ ] This script will test timestamps."
 echo "[ ] First it will create a folder to store the logs."
 mkdir -p ./logs
-echo "[ ] Creating first file. This will be called testfile and stored in the current directory."
+echo "[ ] Creating the test file. This will be called testfile and stored in the current directory."
 echo "Nothing of value" > ./testfile && echo "Testfile created at: $(date)" > ./logs/timelog.txt && stat ./testfile >> ./logs/timelog.txt && echo "--------------------------" >> ./logs/timelog.txt
 echo "[ ] Testfile created. The timestamps on FILE CREATION are:" && echo
 stat ./testfile
@@ -14,16 +14,19 @@ echo "[ ] Accessing the file now."
 cat ./testfile >> /dev/null && echo "Testfile accessed at $(date)" >> ./logs/timelog.txt && stat ./testfile >> ./logs/timelog.txt && echo "--------------------------" >> ./logs/timelog.txt
 echo "[ ] Testfile accessed. The timestamps on FILE ACCESS are:" && echo
 stat ./testfile
+sleep 1
 
 echo "[ ] Modifying file contents now."
 echo "Adding more content to the file. Because we like adding more content to the file. Because it is fun" >> ./testfile && echo "Testfile modified at $(date)" >> ./logs/timelog.txt && stat ./testfile >> ./logs/timelog.txt && echo "--------------------------" >> ./logs/timelog.txt
 echo "[ ] Testfile content modified. The timestamps on FILE MODIFICATION are:" && echo
 stat ./testfile
+sleep 1
 
 echo "[ ] Copying the file now."
 cp ./testfile ./newtest && echo "Testfile copied to newtest at $(date). Timestamps here are for newtest." >> ./logs/timelog.txt && stat ./newtest >> ./logs/timelog.txt && echo "--------------------------" >> ./logs/timelog.txt
 echo "[ ] Testfile copied to newtest. Timestamps on FILE COPY - using newtest - are:" && echo
 stat ./newtest && rm ./newtest
+sleep 1
 
 echo "[ ] Move and rename are identical. Moving the file to a new name"
 mv ./testfile ./renamed && echo "Testfile moved to renamed at $(date). Timestamps here are for renamed." >> ./logs/timelog.txt && stat ./renamed >> ./logs/timelog.txt && echo "--------------------------" >> ./logs/timelog.txt
