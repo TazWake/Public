ece3f05e6aa61dc165fdd9d8c392f62391a957f8
Taz Wake <t.wake@halkynconsulting.co.uk>
2024-03-11 11:43:40 +0000

    Create journalTriage.sh

---
+++ Bash/journalTriage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will run some basic checks aganinst the system journal to see
+# if there are any clear indications of malicious activity.
+
+
+
+
+# Check for invalid SSH logins and write to file.
+
+
+
